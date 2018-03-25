8b9e589888be35cc36b78ea7c24d92afadf570ac
Adam Dolaher <adam@Adams-MacBook-Air.local>
2018-03-25 14:25:05 -0400

    Added control panel functionality to switch off layers.

--- mapping.py
+++ mapping.py
@@ -16,15 +16,19 @@ def colorize(elev):
 
 map = folium.Map(location=[42.260025,-71.464590], zoom_start=6, tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name="Map")
+fg = folium.FeatureGroup(name="Volcano Locations")
 
 for lat, lon, el in zip(latitude, longitude, elevation):
     fg.add_child(folium.CircleMarker(location=[lat,lon], radius = 7, popup=str(el)+" m",
     fill_color=colorize(el), color = "grey", fill=True, fill_opacity=0.5))
 
-fg.add_child(folium.GeoJson(data=open('pop_data.json', 'r', encoding='utf-8-sig').read(),
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('pop_data.json', 'r', encoding='utf-8-sig').read(),
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map.html")
